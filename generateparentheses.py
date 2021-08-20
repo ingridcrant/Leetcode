c9c026caf50a742836900d35333e74e7ea9ab913
ingridcrant <ingridcrant@gmail.com>
2021-08-20 16:03:13 -0400

    generate parentheses problem

---
+++ generateparentheses.py
@@ -0,0 +1,21 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+"""
+
+def generateParenthesis(n):
+    # dynammic programming approach
+    dp = [['']] + [[] for _ in range(n)]
+    for i in range(n + 1):
+        for j in range(i):
+            for x in dp[j]:
+                for y in dp[i - j - 1]:
+                    dp[i].append('(' + x + ')' + y)
+    return dp[n]
+
+# test case
+n = 5
+print(generateParenthesis(n))
